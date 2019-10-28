6964fff8f827314ef83501b373cd53fcd7b5e3ba
Horea <horea.bucerzan@gmail.com>
2019-10-28 22:59:49 +0200

    fixes

--- src/main/java/libgdx/resources/Resource.java
+++ src/main/java/libgdx/resources/Resource.java
@@ -20,6 +20,7 @@ public enum Resource implements Res {
     btn_campaign_disabled("game/campaign/buttons/btn_campaign_disabled.png", Texture.class),
     btn_current_level_down("game/campaign/buttons/btn_current_level_down.png", Texture.class),
     btn_current_level_up("game/campaign/buttons/btn_current_level_up.png", Texture.class),
+    question_background("game/question_background.png", Texture.class),
 
     labels_cs("labels/labels_cs", I18NBundle.class),
     labels_da("labels/labels_da", I18NBundle.class),
